167cc1937fa38498cb0cc02ec352c42dfa9993e5
josephvalenti <125940659+josephvalenti@users.noreply.github.com>
2024-04-21 15:38:46 -0400

    Update db.py

--- db.py
+++ db.py
@@ -1,1 +1,38 @@
 
+// Need API
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Simple Map</title>
+    <script src="https://maps.googleapis.com/maps/api/js?key=YOUR_API_KEY&callback=initMap"
+    async defer></script>
+    <script>
+    var map;
+    function initMap() {
+        map = new google.maps.Map(document.getElementById('map'), {
+            center: {lat: <!DOCTYPE html>
+<html>
+<head>
+    <title>Simple Map</title>
+    <script src="https://maps.googleapis.com/maps/api/js?key=YOUR_API_KEY&callback=initMap"
+    async defer></script>
+    <script>
+    var map;
+    function in_Map() {
+        map = new google.maps.Map(document.getElementById('map'), {
+            center: {lat: 42.4427012, lng: -76.5189745},
+            zoom: 8
+        });
+
+        ensure_marker = new google.maps.Marker({
+            position: {lat: 42.4427012, lng: -76.5189745},
+            map: map,
+            title: 'Hello World!'
+        });
+    }
+    </script>
+</head>
+<body>
+    <div id="map" style="height: 500px; width: 100%;"></div>
+</body>
+</html>
