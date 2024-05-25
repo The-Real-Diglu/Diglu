5f2630d991bbee3ed0a16f40f531c6094200e551
carsonwolber <ctw54@cornell.edu>
2024-05-25 12:12:00 -0400

    ui update + dummy button

--- script.js
+++ script.js
@@ -38,3 +38,17 @@ function addEventToEntries(event) {
   `;
   sideElement.appendChild(entry);
 }
+
+document.getElementById('demobutton').addEventListener('click', function () {
+  document.getElementById('popup').style.display = 'block';
+});
+
+document.getElementById('closePopup').addEventListener('click', function () {
+  document.getElementById('popup').style.display = 'none';
+});
+
+window.addEventListener('click', function (event) {
+  if (event.target == document.getElementById('popup')) {
+    document.getElementById('popup').style.display = 'none';
+  }
+});
